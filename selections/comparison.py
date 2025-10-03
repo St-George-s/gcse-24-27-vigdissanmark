31b1d3d5db07dcea8294e999e3f1eea4f641f04f
vigdissanmark <san021@stge.org.uk>
2025-10-03 12:14:08 +0000

    homework

---
+++ selections/comparison.py
@@ -0,0 +1,41 @@
+# #1
+# firstNumber = float(input("number: "))
+# secondNumber = float(input("number: "))
+# if firstNumber == secondNumber:
+#     print("numbers are equal")
+
+# #2
+# firstNumber = float(input("number: "))
+# secondNumber = float(input("number: "))
+# if firstNumber > secondNumber:
+#     print("the first number is bigger")
+# elif firstNumber < secondNumber:
+#     print("the second number is bigger")
+
+#3
+# firstNumber = float(input("number: "))
+# secondNumber = float(input("number: "))
+# print(firstNumber % secondNumber)
+
+#4
+# number = float(input("number: "))
+# if number > 0:
+#     print("positive")
+# elif number < 0:
+#     print("negative")
+# else:
+#     print("Equal 0")
+
+#5
+# breadth = float(input("number: "))
+# length = float(input("number: "))
+# area = length*breadth
+# print("area is " + str(area) + " metres squared")
+
+#6
+# firstNumber = float(input("number: "))
+# secondNumber = float(input("number: "))
+# if firstNumber % secondNumber == 0:
+#     print("first number is a multiple of the second")
+# else:
+#     print("its not a muktple")
--- selections/logic.py
+++ selections/logic.py
@@ -5,3 +5,40 @@
 #     print("it is sunny")
 
 #question 2
+# temprature = float(input("what is the temprature?: "))
+# rainy = input("enter yes or no: ")
+# if temprature > 15.0 or rainy == "yes":
+#      print("it is rainy or over 15 degrees")
+
+#question 3 
+# apples = int(input("how many apples is there?: "))
+# if apples > 10:
+#     print("the number of apples is over 10")
+# else:
+#     print("the number of apples is under 10")
+
+#question 4 
+# age = int(input("what is your age?: "))
+# driversLicence = input("enter yes or no: ")
+# if age > 18 or driversLicence == "yes":
+#      print("you are over 18 and have a drivers licence")
+# else:
+#      print("you do not have a drivers licence")
+
+#question 5 
+# carSpeed = float(input("what is the speed?: "))
+# weather = input("enter weather: ")
+# if carSpeed > 60.0 or weather != "rain":
+#       print("it is not rainy or the car speed is over 60")
+
+#queston 6
+# hours = int(input("how many hours did you study?: "))
+# prepared = input("yes or no: ")
+# if hours > 5 or prepared == "yes":
+#     print("fhtdydsrty")
+
+#9
+weather = input("is it sunny?: ")
+weekend = input("is it the weekend?: ")
+if weather == "yes" and weekend == "yes" or weather == "no" and weekend == "no":
+    print("condition met")
