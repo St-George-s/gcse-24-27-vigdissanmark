8a3932baf3e41587b148310bf626b8be088888b4
vigdissanmark <san021@stge.org.uk>
2025-11-20 12:03:22 +0000

    idk

---
+++ December test revisions/additionalPrograms.py
@@ -0,0 +1,25 @@
+#q1
+# import random
+ 
+# myRandomNumber = random.randint(1, 3)
+# print(myRandomNumber)
+
+#q1b
+# import random
+ 
+# myRandomNumber = random.randint(1, 3)
+# if myRandomNumber == 1:
+#     print("walk 10km")
+# elif myRandomNumber == 2:
+#     print("run 5km")
+# elif myRandomNumber == 3:
+#     print("swim 1km")
+
+#q2
+counta = 0
+
+while counta <3:
+    word = input("word?: ")
+    for letter in word:
+        if letter == "a" or letter == "A":
+            counta = counta + 1 
---
+++ December test revisions/heights.py
@@ -0,0 +1,9 @@
+# totalHeight = 0
+# counter = 0
+
+# for i in range(3):
+#     height = int(input("how tall are you?: "))
+#     totalHeight = totalHeight + height
+#     counter = counter + 1
+
+# print("average is" , totalHeight / counter)
