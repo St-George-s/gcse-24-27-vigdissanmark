21e263927c3495b782c03ee39ba99df91cdf6b75
vigdissanmark <san021@stge.org.uk>
2026-06-09 09:22:03 +0000

    funcs

---
+++ functions and procedures/functionsAndProcedures.py
@@ -0,0 +1,21 @@
+#procedure with one parameter name
+# def printName(name):
+#     print(name)
+
+# #Function with one parameter name
+# def printNameFunc(name):
+#     return name
+
+# #call the procedure
+# printName("Sanveen")
+
+# #call the function and print a returned value
+# print(printNameFunc("Vichy"))
+
+# #call the function and store returned value in a variable
+# returnedName = printNameFunc("Emby")
+# print(returnedName)
+
+def sphere_volume(radius):
+    sphere_volume = 4/3*3.14*radius**3 
+    print(sphere_volume)
