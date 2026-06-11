2720191fb6a427c0c7423d567ae195ad5d52a9f6
vigdissanmark <san021@stge.org.uk>
2026-06-11 10:59:15 +0000

    aaaaa

--- functions and procedures/functionsAndProcedures.py
+++ functions and procedures/functionsAndProcedures.py
@@ -16,6 +16,21 @@
 # returnedName = printNameFunc("Emby")
 # print(returnedName)
 
-def sphere_volume(radius):
-    sphere_volume = 4/3*3.14*radius**3 
-    print(sphere_volume)
+#q4
+# def sphere_volume(radius):
+#     sphere_volume = (4/3 * 3.14 * (radius)**3)
+#     return sphere_volume
+
+# print(sphere_volume(67))
+
+# volume = sphere_volume(5) 
+# print(volume)
+
+def linear_search(data_list, target):
+    for data_list in data_list:
+        if data_list == target:
+            return True
+    return False
+
+data_list = [3, 8, 2, 10, 7]
+print(linear_search(data_list, 10))
